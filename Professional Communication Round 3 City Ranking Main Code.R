7a33dce3ecd0dfec0dd32c5fa4c45c367998f7b3
JiyaYUN <142545110+JiyaYUN@users.noreply.github.com>
2023-11-12 15:48:56 -0500

    Update Professional Communication Round 3 City Ranking Main Code.R

--- Professional Communication Round 3 City Ranking Main Code.R
+++ Professional Communication Round 3 City Ranking Main Code.R
@@ -2,6 +2,7 @@ remove(list=ls())
 
 library(httr)
 library(readr)
+library(dplyr)
 
 #load data
 url_cities <- "https://raw.githubusercontent.com/JiyaYUN/Professional-Communication-Round-3-City-Ranking/main/cities.csv"
@@ -17,6 +18,27 @@ qualityoflife <- read.csv(text = content(qualityoflife_response, "text"), fill =
 
 #Safety.Rating
 qualityoflife$Safety.Rating <- 100-qualityoflife$Crime.Rating
+
+#City.Environment
+qualityoflife$City.Environment <- 100-qualityoflife$Pollution
+
+#Entertainment
+costofliving$Entertainment <- costofliving$Cappuccino + costofliving$Cinema +
+                              costofliving$Gasoline + costofliving$Wine
+mean(costofliving$Entertainment)
+benchmark_score <- costofliving$Entertainment[costofliving$City == "Nottingham"]
+
+costofliving$Entertainment.Score <- (costofliving$Entertainment / benchmark_score) * 50
+
+qualityoflife$Ranking <- 0.1*qualityoflife$Purchase.Power + 0.1* qualityoflife$Health.Care +
+                         0.1*qualityoflife$Safety.Rating + 0.1* qualityoflife$City.Environment +
+                         0.2*costofliving$Entertainment.Score + 0.4*qualityoflife$Quality.of.Life
+
+write.csv(qualityoflife, "D:/Simon.UR/Fall A/MGC461 Professional Communication/Round 3 Presentation/qualityoflife.csv", row.names = FALSE)
+
+ggplot(data = qualityoflife, aes(x = Purchase.Power)) +
+     geom_histogram(binwidth = 1, fill = "blue", color = "black") +
+     labs(title = "Distribution of Ratings", x = "Purchase.Power", y = "Frequency")
 
 library(ggplot2)
 ggplot(data = qualityoflife, aes(x = Safety.Rating)) +
@@ -44,5 +66,3 @@ ggplot(data = costofliving, aes(x = Cappuccino)) +
 ggplot(data = costofliving, aes(x = Wine)) +
      geom_histogram(binwidth = 1, fill = "blue", color = "black") +
      labs(title = "Distribution of Ratings", x = "Wine", y = "Frequency")
-
-costofliving$SocialNeeds <- costofliving$Cappuccino + costofliving$Cinema + costofliving$Wine
